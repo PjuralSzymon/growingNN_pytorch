86345e2e58cba726a4fb665a59f2b844200cd187
szymon <pjuralszymqn@gmail.com>
2026-05-10 17:15:44 +0200

    feat(R5) Adding deelte neurons placeholder

---
+++ growingnn/actions/delete_neurons.py
@@ -0,0 +1,26 @@
+
+from typing import List
+
+from torch import fx, nn
+from .action import Action, Layer_Type
+
+from growingnn.actions.utils.layer_Factory import LinearFactory
+from growingnn.actions.utils.model_analyser import get_all_hidden_modules, module_sequential_pairs
+from growingnn.actions.utils.name_factory import unique_call_module_name
+from growingnn.actions.utils.model_transformations import add_new_residual_layer, add_new_seq_layer, delete_layer
+from .action import Action, Layer_Type
+
+class DelNeurons(Action):
+    def execute(self, model: nn.Module | fx.GraphModule):
+        pass
+
+    def can_be_infulenced(self, by_action):
+        return False
+
+    @staticmethod
+    def generate_all_actions(model: nn.Module | fx.GraphModule) -> List[Action]:
+        actions = []
+
+
+    def __str__(self):
+        return " ( Delete Neurons Action: " + str(self.params) + " ) "
